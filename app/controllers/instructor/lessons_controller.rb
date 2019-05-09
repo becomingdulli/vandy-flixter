f658798571d918a297c3d2dc29472462ad67704b
Jason <becomingdulli@gmail.com>
2019-05-09 23:37:33 +0000

    Lesson 32 day 6

--- app/controllers/instructor/lessons_controller.rb
+++ app/controllers/instructor/lessons_controller.rb
@@ -20,27 +20,27 @@ class Instructor::LessonsController < ApplicationController
   private
 
   def require_authorized_for_current_lesson
-    if current_lesson.section.course.user != current_user
-      render plain: 'Unauthorized', status: :unauthorized
-    end
-  end
+     if current_lesson.section.course.user != current_user
+       render plain: 'Unauthorized', status: :unauthorized
+     end
+   end
 
-  def current_lesson
-    @current_lesson ||= Lesson.find(params[:id])
-  end
+   def current_lesson
+     @current_lesson ||= Lesson.find(params[:id])
+   end
 
-  def require_authorized_for_current_section
-    if current_section.course.user != current_user
-      render plain: 'Unauthorized', status: :unauthorized
-    end
-  end
+   def require_authorized_for_current_section
+     if current_section.course.user != current_user
+       render plain: 'Unauthorized', status: :unauthorized
+     end
+   end
 
-  helper_method :current_section
-  def current_section
-    @current_section ||= Section.find(params[:section_id])
-  end
+   helper_method :current_section
+   def current_section
+     @current_section ||= Section.find(params[:section_id])
+   end
 
-  def lesson_params
-    params.require(:lesson).permit(:title, :subtitle, :video, :row_order_position)
-  end
-end
+   def lesson_params
+     params.require(:lesson).permit(:title, :subtitle, :video, :row_order_position)
+   end
+ end
--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,8 +1,9 @@
 class LessonsController < ApplicationController
   before_action :authenticate_user!
-  
+
   def show
   end
+
   private
 
   helper_method :current_lesson
